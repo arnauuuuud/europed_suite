3657d15d1fb4aa3b50a2652c0b6f37d0bac407c2
Arno <arnaud.lafay@gmail.com>
2025-03-14 13:54:53 +0000

    add run_sequentially_waitinglist and batchpython

---
+++ batchpython.py
@@ -0,0 +1,26 @@
+#!/usr/local/depot/Python-3.7/bin/python3
+from jetlib.misc import BatchRun
+import sys
+import os
+# First argument should be filepath of python file to run in batch
+# All additional arguments will be handed to the python file as cmdline arguments
+filepath = sys.argv[1]
+if '/' not in filepath:
+    filepath = f"{os.getcwd()}/{filepath}"
+
+username = 'jwp9427'
+print(f'Current username is {jwp9427}, if wrong please change it')
+initdir = f"/home/{username}/work/scripts/batchtmpdir/"
+
+runfile_name = filepath.split('/')[-1][:-3]
+submitfile_name = runfile_name
+
+runfile_argument = f"python {filepath}"
+if len(sys.argv) > 2: # appending possible command line specifiers
+    runfile_argument = f"{runfile_argument} {' '.join(sys.argv[2:])}"
+
+runfile_arguments = [runfile_argument]
+submitfile_arguments = ["# @ requirements = (Memory = any)\n"]
+
+batch = BatchRun(initdir, runfile_name, submitfile_name, runfile_arguments, submitfile_arguments, cleanup = False, email = True)
+batch.submit()
---
+++ run_sequentially_waitinglist.py
@@ -0,0 +1,85 @@
+#!/usr/local/depot/Python-3.7/bin/python
+# /usr/local/depot/Python-3.5.1/bin/python
+
+import subprocess
+import time
+import fcntl
+
+username = 'jwp9427'
+print(f'Current username is {username}, if not good, please update it.')
+
+waitinglistfile = f'/home/{username}/Desktop/waitinglist'
+launchedlist_file = f'/home/{username}/Desktop/launched'
+
+def lock_file(file):
+    fcntl.flock(file, fcntl.LOCK_EX)
+
+def unlock_file(file):
+    fcntl.flock(file, fcntl.LOCK_UN)
+
+#####################################################################
+def read_and_delete_first_line(file_path):
+    with open(file_path, 'r') as file:
+        lock_file(file)
+        lines = file.readlines()
+        unlock_file(file)
+    if not lines:
+        return None  # File is empty
+    first_line = lines[0]
+
+    if first_line == 'pause\n':
+        return first_line
+    else:
+        with open(file_path, 'w') as file:
+            lock_file(file)
+            file.writelines(lines[1:])
+            unlock_file(file)
+        return first_line
+
+
+def append(file_path,line):
+    with open(file_path, 'a') as file:
+        lock_file(file)
+        file.write(line)
+        unlock_file(file)
+
+
+def count_processes_running():
+    command = "llq -l | grep {username}.*"
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)   
+    return len(result.stdout.splitlines())
+
+
+def launch_and_wait(filename):
+    print(f'Launching run with filename: {filename}')
+    command = f"/home/{username}/work/europed/europed.py /home/{username}/work/europed/input/{filename} -b"
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+
+    time.sleep(600)
+    while count_processes_running()>15:
+        print("Not enough computing time to launch new runs", end='\r')
+        time.sleep(300)  # Check every 300 second  
+    print("Available computing time                                       ")
+    return True
+
+def main():
+    while True:
+        filename = read_and_delete_first_line(waitinglistfile)
+        while filename is not None and filename != 'pause\n' :
+            append(launchedlist_file,filename)
+            print(filename[:-1])
+            launch_and_wait(filename[:-1])
+            filename = read_and_delete_first_line(waitinglistfile)
+        if filename is None:
+            print('Empty waiting list - wait 1 hour')
+            time.sleep(3600)
+        elif filename == 'pause\n':
+            print('Pause - wait 10 minutes')
+            time.sleep(600)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
